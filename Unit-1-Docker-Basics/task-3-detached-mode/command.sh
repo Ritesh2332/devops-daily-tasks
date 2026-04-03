5b5dc002b41d3985dfc432ace939e6a2be778042
Ritesh Kumar Paswan <riteshkumarpaswan538@gmail.com>
2026-04-03 23:52:08 +0530

    Task 3 pending

---
+++ Unit-1-Docker-Basics/task-3-detached-mode/command.sh
@@ -0,0 +1,25 @@
+# Run container in detached mode
+docker run -dit --name detached-container ubuntu bash
+
+# Check running containers
+docker ps
+
+# Run persistent container
+docker run -dit --name persistent-container ubuntu sleep infinity
+
+# Verify
+docker ps
+
+# Access container
+docker exec -it persistent-container bash
+
+# Exit
+exit
+
+# Stop containers
+docker stop detached-container
+docker stop persistent-container
+
+# Remove containers
+docker rm detached-container
+docker rm persistent-container
